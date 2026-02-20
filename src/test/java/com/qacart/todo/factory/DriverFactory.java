875ea6d95cdc105e7fe5be3569644c662d5c6057
Michael Zarif <michaelzsobhy@gmail.com>
2026-02-20 15:02:30 +0200

    Support multiple browsers in DriverFactory
    Read a 'browser' system property and instantiate the appropriate WebDriver (Chrome, Firefox, Edge, Safari) using a switch expression. Added necessary imports and a default error for unsupported browsers; retains window sizing and implicit wait. This enables running tests on different browsers via -Dbrowser=<name> (defaults to Chrome).

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -2,13 +2,25 @@ package com.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class DriverFactory {
 
     public WebDriver initializeDriver() {
-        WebDriver driver = new ChromeDriver();
+
+        String browser = System.getProperty("browser", "Chrome");   // Gets the system property when sent from terminal
+        WebDriver driver = switch (browser) {
+            case "Chrome" -> new ChromeDriver();
+            case "Firefox" -> new FirefoxDriver();
+            case "Edge" -> new EdgeDriver();
+            case "Safari" -> new SafariDriver();
+            default -> throw new RuntimeException("The browser is not supported");
+        };
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
