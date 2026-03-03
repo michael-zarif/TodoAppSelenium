662da6ed30ee8cb26f59038123bc6c5e67821bf2
Michael Zarif <michaelzsobhy@gmail.com>
2026-03-03 18:11:56 +0200

    Integrate Allure reporting in tests
    Add allure-testng (v2.29.0) to pom.xml and instrument tests for richer Allure reports. LoginTest and TodoItemTest were updated to include Allure imports and annotations (@Feature on classes, @Story and @Description on test methods) and test descriptions to improve reporting. No functional test logic changed.

--- src/test/java/com/qacart/todo/testcases/LoginTest.java
+++ src/test/java/com/qacart/todo/testcases/LoginTest.java
@@ -3,12 +3,18 @@ package com.qacart.todo.testcases;
 import com.qacart.todo.base.BaseTest;
 import com.qacart.todo.pages.LoginPage;
 import com.qacart.todo.utils.ConfigUtils;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Feature("Auth Feature")
 public class LoginTest extends BaseTest {
 
-    @Test
+    @Story("Login with email and password")
+    @Description("It will login by filling the email and password and navigate to the Todo page")
+    @Test(description = "Test the Login functionality using email and password")
     public void ShouldBeAbleToLoginWithValidCredentials() {
         LoginPage loginPage = new LoginPage(driver);
         String welcomeMessage = loginPage.load()
--- src/test/java/com/qacart/todo/testcases/TodoItemTest.java
+++ src/test/java/com/qacart/todo/testcases/TodoItemTest.java
@@ -6,11 +6,17 @@ import com.qacart.todo.base.BaseTest;
 import com.qacart.todo.pages.LoginPage;
 import com.qacart.todo.pages.TodoPage;
 import com.qacart.todo.utils.ConfigUtils;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Feature("Todo Feature")
 public class TodoItemTest extends BaseTest {
-    @Test
+    @Story("Add new Todo item")
+    @Description("It will add a new Todo item by filling the value and clicking add")
+    @Test(description = "Test Adding Todo item functionality")
     public void ShouldBeAbleToAddTodoItem() {
         RegisterApi registerApi = new RegisterApi();
         registerApi.register();
@@ -25,7 +31,9 @@ public class TodoItemTest extends BaseTest {
         Assert.assertEquals(todoItemText, "Learn Selenium");
     }
 
-    @Test
+    @Story("Delete Todo item")
+    @Description("It will delete a Todo item and checking for deletion")
+    @Test(description = "Test Deleting Todo item functionality")
     public void ShouldBeAbleToDeleteTodoItem(){
         RegisterApi registerApi = new RegisterApi();
         registerApi.register();
