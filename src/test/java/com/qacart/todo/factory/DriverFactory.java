f0053018782e15957052199645105f2325bfd359
Michael Zarif <48383327+michael-zarif@users.noreply.github.com>
2026-05-05 01:41:44 +0300

    Revert DriverFactory Headless Changes

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -2,7 +2,6 @@ package com.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -14,20 +13,13 @@ public class DriverFactory {
     public WebDriver initializeDriver() {
 
         String browser = System.getProperty("browser", "Chrome");   // Gets the system property when sent from terminal
-        WebDriver driver = null;
-        switch (browser) {
-            case "Chrome" -> {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless=new");
-                options.addArguments("--no-sandbox");
-                options.addArguments("--disable-dev-shm-usage");
-                driver = new ChromeDriver(options);
-            }
-            case "Firefox" -> driver = new FirefoxDriver();
-            case "Edge" -> driver = new EdgeDriver();
-            case "Safari" -> driver = new SafariDriver();
+        WebDriver driver = switch (browser) {
+            case "Chrome" -> new ChromeDriver();
+            case "Firefox" -> new FirefoxDriver();
+            case "Edge" -> new EdgeDriver();
+            case "Safari" -> new SafariDriver();
             default -> throw new RuntimeException("The browser is not supported");
-        }
+        };
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
